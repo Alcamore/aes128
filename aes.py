38921644274a0d833d05202de8f1fea44431fe48
Jadon Hansell <alcamore@gmail.com>
2019-12-01 18:16:55 -0800

    Move functions to a class & mostly finish code

--- aes.py
+++ aes.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-nk = 4  # key length in words
-nb = 4  # block size in words
-nr = 10 # number of rounds
-
 # Substitution box for the sub_bytes routine for encryption
-sub_box = [
+_sub_box = [
     [0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76],
     [0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0],
     [0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15],
@@ -20,11 +16,11 @@ sub_box = [
     [0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a],
     [0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e],
     [0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf],
-    [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
+    [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16],
 ]
 
 # Substitution box for the inv_sub_bytes routine for decryption
-inv_sub_box = [
+_inv_sub_box = [
     [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
     [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
     [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
@@ -40,162 +36,288 @@ inv_sub_box = [
     [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
     [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
     [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
-    [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
+    [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d],
 ]
 
-# Power of 2 for use in key expansion
-Rcon = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a]
+def cipher(plaintext, key):
+    a = AES()
 
-def cipher(input, output, word):
-    pass
+    state = plaintext[:]
+    key_schedule = a.key_expansion(key)
 
-def decipher(input, output, word):
-    pass
+    a.add_round_key(state, key_schedule[:a.block_size])
 
-def sub_bytes(state):
-    '''
-        Perform byte substitution operation
-    '''
-    new_state = bytearray(state)
+    for round in range(a.num_rounds):
+        state = a.sub_bytes(state)
+        state = a.shift_rows(state)
+        state = a.mix_columns(state)
+        key_start = round * a.block_size
+        key_end = (round+ 1 ) * a.block_size - 1
+        a.add_round_key(state, key_schedule[key_start:key_end])
 
-    for index, byte in enumerate(state):
-        # separate hex xy into x and y
-        x = byte // 0x10
-        y = byte % 0x10
+    state = a.sub_bytes(state)
+    state = a.shift_rows(state)
+    key_start = a.num_rounds * a.block_size
+    key_end = (a.num_rounds + 1) * a.block_size - 1
+    a.add_round_key(state, key_schedule[key_start:key_end])
 
-        # perform substitution
-        new_state[index] = sub_box[x][y]
+    return state
 
-    return bytes(new_state)
+def decipher(ciphertext, key):
+    a = AES()
 
-def inv_sub_bytes(state):
-    '''
-        Perform inverse of the byte substitution operation
-    '''
-    new_state = bytearray(state)
+    state = ciphertext[:]
+    key_schedule = a.key_expansion(key)
 
-    for index, byte in enumerate(state):
-        # separate hex xy into x and y
-        x = byte // 0x10
-        y = byte % 0x10
+    key_start = a.num_rounds * a.block_size
+    key_end = (a.num_rounds + 1) * a.block_size - 1
+    a.add_round_key(state, key_schedule[key_start:key_end])
 
-        # perform substitution
-        new_state[index] = inv_sub_box[x][y]
+    for round in range(a.num_rounds-1, 1, -1):
+        state = a.inv_shift_rows(state)
+        state = a.inv_sub_bytes(state)
+        key_start = round * a.block_size
+        key_end = (round + 1) * a.block_size - 1
+        a.add_round_key(state, key_schedule[key_start:key_end])
+        state = a.inv_mix_columns(state)
 
-    return new_state
+    state = a.inv_shift_rows(state)
+    state = a.inv_sub_bytes(state)
+    key_start = a.num_rounds * a.block_size
+    key_end = (a.num_rounds + 1) * a.block_size - 1
+    a.add_round_key(state, key_schedule[key_start:key_end])
 
-def shift_rows(state: bytes):
-    '''
-        Perform inverse of the shift rows operation
-    '''
-    # Convert state to a matrix
-    mtx = matrix(state)
+    return state
 
-    # Shift rows 1 2 and 3 back by 1, 2, and 3 respectively
-    mtx[1] = mtx[1][1:] + mtx[1][:1]
-    mtx[2] = mtx[2][2:] + mtx[2][:2]
-    mtx[3] = mtx[3][3:] + mtx[3][:3]
+class AES:
 
-    # Convert result back to an array
-    return bytes(inv_matrix(mtx))
+    def __init__(self):
+        self.key_length = 4 # key length in words
+        self.block_size = 4 # block size in words
+        self.num_rounds = 10 # number of rounds to perform
 
-def inv_shift_rows(state: bytes):
-    '''
-        Perform inverse of the shift rows operation
-    '''
-    # Convert state to a matrix
-    mtx = matrix(state)
+        # populate round constant list with powers of 2 in GF(8)
+        self.round_constant = [0x1]
+        for i in range(14):
+            self.round_constant.append(self.xmult(self.round_constant[i], 2))
 
-    # Shift rows 1 2 and 3 back by 1, 2, and 3 respectively
-    mtx[1] = mtx[1][-1:] + mtx[1][:-1]
-    mtx[2] = mtx[2][-2:] + mtx[2][:-2]
-    mtx[3] = mtx[3][-3:] + mtx[3][:-3]
+    def sub_bytes(self, state):
+        '''Performs the byte substitution operation.
+        '''
+        new_state = bytearray(state)
 
-    # Convert result back to an array
-    return bytes(inv_matrix(mtx))
+        for index, byte in enumerate(state):
+            # separate hex xy into x and y
+            x = byte // 0x10
+            y = byte % 0x10
 
-def mix_columns(state: bytes):
-    '''
-        Perform mix columns operation
-    '''
-    # Make a list of all the columns in the state
-    cols = [list(state[:4]), list(state[4:8]), list(state[8:12]), list(state[12:])]
+            # perform substitution
+            new_state[index] = _sub_box[x][y]
 
-    # Perform multiplication
-    for col in cols:
-        tmp = list(col)
-        col[0] = (2*tmp[0] ^ 3*tmp[1] ^ tmp[2] ^ tmp[3]) % 255
-        col[1] = (tmp[0] ^ 2*tmp[1] ^ 3*tmp[2] ^ tmp[3]) % 255
-        col[2] = (tmp[0] ^ tmp[1] ^ 2*tmp[2] ^ 3*tmp[3]) % 255
-        col[3] = (3*tmp[0] ^ tmp[1] ^ tmp[2] ^ 2*tmp[3]) % 255
+        return bytes(new_state)
 
-    # Re-combine list
-    return bytes(cols[0] + cols[1] + cols[2] + cols[3])
+    def inv_sub_bytes(self, state):
+        '''Performs the inverse of the byte substitution operation.
+        '''
+        new_state = bytearray(state)
 
+        for index, byte in enumerate(state):
+            # separate hex xy into x and y
+            x = byte // 0x10
+            y = byte % 0x10
 
+            # perform substitution
+            new_state[index] = _inv_sub_box[x][y]
 
-def inv_mix_columns(state: bytes):
-    '''
-        Perform inverse mix columns operation
-    '''
-    # Make a list of all the columns in the state
-    cols = [list(state[:4]), list(state[4:8]), list(state[8:12]), list(state[12:])]
+        return new_state
 
-    for col in cols:
+    def shift_rows(self, state: bytes):
+        '''Performs the inverse of the shift rows operation.
+        '''
+        # Convert state to a matrix
+        matrix = self.make_matrix(state)
+
+        # Shift rows 1 2 and 3 back by 1, 2, and 3 respectively
+        for index, row in enumerate(matrix):
+            matrix[index] = self.rotate(row, index)
+
+        # Convert result back to an array
+        return bytes(self.inv_make_matrix(matrix))
+
+    def inv_shift_rows(self, state):
+        '''Performs the inverse of the shift rows operation.
+        '''
+        # Convert state to a matrix
+        matrix = self.make_matrix(state)
+
+        # Shift each row by their row number respectively
+        for index, row in enumerate(matrix):
+            matrix[index] = self.rotate(row, index, reverse=True)
+
+        # Convert result back to an array
+        return bytes(self.inv_make_matrix(matrix))
+
+    def mix_columns(self, state):
+        '''Performs the mix columns operation.
+        '''
+        # Make a list of all the columns in the state
+        cols = self.make_column(bytearray(state))
+
+        xmult = self.xmult
         # Perform multiplication
-        tmp = list(col)
-        col[0] = (0x0e*tmp[0] ^ 0x0b*tmp[1] ^ 0x0d*tmp[2] ^ 0x09*tmp[3]) % 255
-        col[1] = (0x09*tmp[0] ^ 0x0e*tmp[1] ^ 0x0b*tmp[2] ^ 0x0d*tmp[3]) % 255
-        col[2] = (0x0d*tmp[0] ^ 0x09*tmp[1] ^ 0x0e*tmp[2] ^ 0x0b*tmp[3]) % 255
-        col[3] = (0x0b*tmp[0] ^ 0x0d*tmp[1] ^ 0x09*tmp[2] ^ 0x0e*tmp[3]) % 255
+        for col in cols:
+            tmp = col[:]
+            col[0] = xmult(2, tmp[0]) ^ xmult(3, tmp[1]) ^ xmult(1, tmp[2]) ^ xmult(1, tmp[3])
+            col[1] = xmult(1, tmp[0]) ^ xmult(2, tmp[1]) ^ xmult(3, tmp[2]) ^ xmult(1, tmp[3])
+            col[2] = xmult(1, tmp[0]) ^ xmult(1, tmp[1]) ^ xmult(2, tmp[2]) ^ xmult(3, tmp[3])
+            col[3] = xmult(3, tmp[0]) ^ xmult(1, tmp[1]) ^ xmult(1, tmp[2]) ^ xmult(2, tmp[3])
 
-    # Re-combine columns
-    return bytes(cols[0] + cols[1] + cols[2] + cols[3])
+        # Re-combine list
+        return self.inv_make_column(cols)
 
-def add_round_key(state: bytes, word):
-    pass
+    def inv_mix_columns(self, state):
+        '''Performs the inverse mix columns operation.
+        '''
+        # Make a list of all the columns in the state
+        cols = self.make_column(bytearray(state))
 
-# def key_expansion(key, w):
-    # word = bytearray(w)
-    # for i in range(4):
-    #     word[i] = [key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]]
+        xmult = self.xmult
+        # Perform multiplication
+        for col in cols:
+            tmp = col[:]
+            col[0] = xmult(0x0e, tmp[0]) ^ xmult(0x0b, tmp[1]) ^ xmult(0x0d, tmp[2]) ^ xmult(0x09, tmp[3])
+            col[1] = xmult(0x09, tmp[0]) ^ xmult(0x0e, tmp[1]) ^ xmult(0x0b, tmp[2]) ^ xmult(0x0d, tmp[3])
+            col[2] = xmult(0x0d, tmp[0]) ^ xmult(0x09, tmp[1]) ^ xmult(0x0e, tmp[2]) ^ xmult(0x0b, tmp[3])
+            col[3] = xmult(0x0b, tmp[0]) ^ xmult(0x0d, tmp[1]) ^ xmult(0x09, tmp[2]) ^ xmult(0x0e, tmp[3])
 
-    # for i in range(4, 44):
-    #     temp = word[i-1]
-    #     if (i % 4 == 0):
-    #         temp = sub_bytes(temp[1:] + temp[:1]) ^ Rcon[i//4]
-    #     word[i] = word[i-4] ^ temp
+        # Re-combine columns`
+        return self.inv_make_column(cols)
+
+    def add_round_key(self, state, key_schedule):
+        '''XORs the state with the next round key.
+        '''
+        # Make a list of all the columns in the state
+        cols = self.make_column(state)
+
+        # Make a matching list from the key schedule segment
+        keys = self.make_column(key_schedule)
+
+        for index, (col, key) in enumerate(zip(cols, keys)):
+            cols[index] = [c^k for c,k in zip(col, key)]
+
+        return self.inv_make_column(cols)
+
+    def key_expansion(self, key):
+        '''Expands the key into a key schedule.
+        '''
+        schedule_size = self.block_size * (self.num_rounds + 1)
+        key_sch = bytearray(schedule_size)
+        temp = bytearray(4)
+
+        for i in range(0, self.key_length, 4):
+            key_sch[i:i+4] = key[4*i:4*i+4]
+
+        for i in range(self.key_length, self.block_size * self.num_rounds, 4):
+            temp = key_sch[i-1:i+3]
+            if (i % self.key_length == 0):
+                temp = self.constant_word_xor(self.sub_bytes(self.rotate(temp)), self.round_constant[i//self.key_length])
+            key_sch[i:i+4] = self.word_xor(key_sch[i-4:i], temp)
+
+        return bytes(key_sch)
 
 
-def matrix(state):
-    '''
-        Converts the state from an array form to a matrix form
-    '''
-    mtx = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ]
+    def make_matrix(self, state):
+        '''Converts the state from an array form to a matrix form.
+        '''
+        mtx = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
 
-    for index, byte in enumerate(state):
-        row = index % 4
-        col = index // 4
-        mtx[row][col] = byte
+        for index, byte in enumerate(state):
+            row = index % 4
+            col = index // 4
+            mtx[row][col] = byte
 
-    return mtx
+        return mtx
 
-def inv_matrix(mtx):
-    '''
-        Converts the state from a matrix form to an array form
-    '''
-    state = bytearray(16)
+    def inv_make_matrix(self, mtx):
+        '''Converts the state from a matrix form to a bytestring form.
+        '''
+        state = bytearray(16)
 
-    for index in range(len(state)):
-        row = index % 4
-        col = index // 4
-        state[index] = mtx[row][col]
-    return bytes(state)
+        for index in range(len(state)):
+            row = index % 4
+            col = index // 4
+            state[index] = mtx[row][col]
+        return bytes(state)
+
+    def make_column(self, state):
+        '''Extracts the columns of the state into a list.
+        '''
+        return [
+            state[:4],
+            state[4:8],
+            state[8:12],
+            state[12:]
+        ]
+
+    def inv_make_column(self, cols):
+        '''Converts a column version of the state into a list.
+        '''
+        return cols[0] + cols[1] + cols[2] + cols[3]
+
+    def xmult(self, a, b):
+        '''Performs a finite-field multiplication in GF(2^8).
+
+        Based on the Russian Peasant Multiplication Algorithm
+        found at https://en.wikipedia.org/wiki/Finite_field_arithmetic
+        '''
+        if a > 255 or b > 255 or a < 0 or b < 0:
+            raise ValueError(f'{a=} and {b=} must be between 0 and 255')
+
+        product = 0
+        polynomial = 0b1_0001_1011 # x^8 + x^4 + x^3 + x + 1
+
+        while a and b:
+            if b & 1 != 0:
+                product ^= a
+            if a & 128:
+                a = (a << 1) ^ polynomial
+            else:
+                a *= 2
+            b //= 2
+
+        return product
+
+    def rotate(self, iterable, amount=1, reverse=False):
+        '''Rotates an iterable by a certain amount.
+
+        Rotates left by default
+        '''
+        if reverse:
+            amount *= -1
+        return iterable[amount:] + iterable[:amount]
+
+    def constant_word_xor(self, word, constant):
+        ''' XORs a word with some constant
+        '''
+        return [x^constant for x in word]
+
+    def word_xor(self, word, other):
+        ''' XORs a word with another word
+        '''
+        return [x^y for x, y in zip(word, other)]
+
 
 if __name__ == '__main__':
-    pass
+    choice = input("Type 'c' for cipher or 'd' for decipher: ")
+    text = input("Type text in hex: ")
+    key = input("Type key in hex: ")
+
+    if choice == 'c':
+        print(cipher(text, key).hex())
+    if choice == 'd':
+        print(decipher(text, key).hex())
--- test_aes.py
+++ test_aes.py
@@ -4,32 +4,156 @@ import aes
 
 class TestAES(unittest.TestCase):
     def setUp(self):
-        plaintext = bytes.fromhex('00112233445566778899aabbccddeeff')
-        key = bytes.fromhex('000102030405060708090a0b0c0d0e0f')
+        self.plaintext = bytes.fromhex('00112233445566778899aabbccddeeff')
+        self.key = bytes.fromhex('000102030405060708090a0b0c0d0e0f')
+        self.ciphertext = bytes.fromhex('69c4e0d86a7b0430d8cdb78070b4c55a')
+
+        self.plaintext2 = bytes.fromhex('54776f204f6e65204e696e652054776f')
+        self.key2 = bytes.fromhex('5468617473206d79204b756e67204675')
+        self.ciphertext2 = bytes.fromhex('29c3505f571420f6402299b31a02d73a')
+
+        self.aes = aes.AES()
+
+    def testCipher(self):
+        ciphertext = aes.cipher(self.plaintext, self.key)
+        ciphertext2 = aes.cipher(self.plaintext2, self.key2)
+        self.assertEqual(ciphertext, self.ciphertext)
+        self.assertEqual(ciphertext2, self.ciphertext2)
+
+    def testDecipher(self):
+        plaintext = aes.decipher(bytes.fromhex(''), self.key)
+        plaintext2 = aes.decipher(bytes.fromhex(''), self.key2)
+        self.assertEqual(plaintext, self.plaintext)
+        self.assertEqual(plaintext2, self.plaintext2)
 
     def testSubBytes(self):
-        state = aes.sub_bytes(bytes.fromhex('00102030405060708090a0b0c0d0e0f0'))
-        self.assertEqual(state, bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
+        state = self.aes.sub_bytes(bytes.fromhex('00102030405060708090a0b0c0d0e0f0'))
+        state2 = self.aes.sub_bytes(bytes.fromhex('001f0e543c4e08596e221b0b4774311a'))
+
+        self.assertEqual(state.hex(), '63cab7040953d051cd60e0e7ba70e18c')
+        self.assertEqual(state2.hex(), '63c0ab20eb2f30cb9f93af2ba092c7a2')
 
     def testShiftRows(self):
-        state = aes.shift_rows(bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
-        self.assertEqual(state, bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
+        state = self.aes.shift_rows(bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
+        self.assertEqual(state.hex(), '6353e08c0960e104cd70b751bacad0e7')
 
     def testMixColumns(self):
-        state = aes.mix_columns(bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
-        self.assertEqual(state, bytes.fromhex('5f72641557f5bc92f7be3b291db9f91a'))
+        state = self.aes.mix_columns(bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
+        self.assertEqual(state.hex(), '5f72641557f5bc92f7be3b291db9f91a')
 
     def testInvSubBytes(self):
-        state = aes.inv_sub_bytes(bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
-        self.assertEqual(state, bytes.fromhex('00102030405060708090a0b0c0d0e0f0'))
+        state = self.aes.inv_sub_bytes(bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
+        self.assertEqual(state.hex(), '00102030405060708090a0b0c0d0e0f0')
 
     def testInvShiftRows(self):
-        state = aes.inv_shift_rows(bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
-        self.assertEqual(state, bytes.fromhex('63cab7040953d051cd60e0e7ba70e18c'))
+        state = self.aes.inv_shift_rows(bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
+        self.assertEqual(state.hex(), '63cab7040953d051cd60e0e7ba70e18c')
 
     def testInvMixColumns(self):
-        state = aes.inv_mix_columns(bytes.fromhex('5f72641557f5bc92f7be3b291db9f91a'))
-        self.assertEqual(state, bytes.fromhex('6353e08c0960e104cd70b751bacad0e7'))
+        state = self.aes.inv_mix_columns(bytes.fromhex('5f72641557f5bc92f7be3b291db9f91a'))
+        self.assertEqual(state.hex(), '6353e08c0960e104cd70b751bacad0e7')
+
+    def testKeyExpansion(self):
+        #key_schedule = self.aes.key_expansion(self.key)
+        key_schedule2 = self.aes.key_expansion(self.key2)
+
+        self.assertEqual(key_schedule2[:16].hex(), '5468617473206d79204b756e67204675')
+        self.assertEqual(key_schedule2[16:32].hex(), 'e232fcf191129188b159e4e6d679a293')
+        self.assertEqual(key_schedule2[32:48].hex(), '56082007c71ab18f76435569a03af7fa')
+        self.assertEqual(key_schedule2[48:64].hex(), 'd2600de7157abc686339e901c3031efb')
+
+    def testAddRoundKey(self):
+        state = bytes.fromhex('54776F204F6E65204E696E652054776F')
+        round_key = bytes.fromhex('5468617473206D79204B756E67204675')
+
+        result = self.aes.add_round_key(state, round_key)
+
+        self.assertEqual(bytes(result).hex(), '001f0e543c4e08596e221b0b4774311a')
+
+class TestAESUtilities(unittest.TestCase):
+    def setUp(self):
+        self.aes = aes.AES()
+
+    def testMakeMatrix(self):
+        input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        matrix = self.aes.make_matrix(input)
+        self.assertEqual(matrix, [
+            [0, 4, 8, 12],
+            [1, 5, 9, 13],
+            [2, 6, 10, 14],
+            [3, 7, 11, 15]
+        ])
+
+    def testInvMakeMatrix(self):
+        input = [
+            [0, 4, 8, 12],
+            [1, 5, 9, 13],
+            [2, 6, 10, 14],
+            [3, 7, 11, 15]
+        ]
+        result = self.aes.inv_make_matrix(input)
+        expected = bytes([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        self.assertEqual(result.hex(), expected.hex())
+
+    def testMakeColumn(self):
+        input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        cols = self.aes.make_column(input)
+
+        self.assertEqual(cols, [
+            [0, 1, 2, 3],
+            [4, 5, 6, 7],
+            [8, 9, 10, 11],
+            [12, 13, 14, 15]
+        ])
+
+    def testInvMakeColumn(self):
+        input = [
+            [0, 1, 2, 3],
+            [4, 5, 6, 7],
+            [8, 9, 10, 11],
+            [12, 13, 14, 15]
+        ]
+        result = self.aes.inv_make_column(input)
+        expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+        self.assertEqual(result, expected)
+
+    def testRotate(self):
+        input = [0, 1, 2, 3]
+
+        rotate_left = self.aes.rotate(input)
+        rotate_right = self.aes.rotate(input, reverse=True)
+        rotate_three_left = self.aes.rotate(input, 3)
+        rotate_three_right = self.aes.rotate(input, 3, reverse=True)
+
+        self.assertEqual(rotate_left, [1, 2, 3, 0])
+        self.assertEqual(rotate_right, [3, 0, 1, 2])
+        self.assertEqual(rotate_three_left, [3, 0, 1, 2])
+        self.assertEqual(rotate_three_right, [1, 2, 3, 0])
+
+    def testConstantWordXOR(self):
+        input = [1, 2, 3, 4]
+        constant = 42
+
+        result = self.aes.constant_word_xor(input, constant)
+
+        self.assertEqual(result, [43, 40, 41, 46])
+
+    def testWordXOR(self):
+        input = [1, 2, 3, 4]
+        other = [4, 3, 2, 1]
+
+        result = self.aes.word_xor(input, other)
+
+        self.assertEqual(result, [5, 1, 1, 5])
+
+    def testXMult(self):
+        self.assertEqual(self.aes.xmult(80, 74), 209)
+
+        with self.assertRaises(ValueError):
+            self.aes.xmult(-1, 0)
+        with self.assertRaises(ValueError):
+            self.aes.xmult(256, 0)
 
 if __name__ == '__main__':
     unittest.main()
